cd27f859ebe38c86cddce97ef3afb04dee87a6e4
DonnaIB <donnaimrie@gmail.com>
2021-10-30 11:13:00 +0000

    Deployed to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn superior_spanish.wsgi:application
--- superior_spanish/settings.py
+++ superior_spanish/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+14d4*qgif@1b71ybp)_*!00#ayqrz^algi267o8nm@*n)pt@@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['superiro-spanish.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -104,21 +104,18 @@ WSGI_APPLICATION = 'superior_spanish.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-""" If DATABASE_URL is in our OS environ then use it to setup the database
-else use the default Django sqlite3 setup.
-"""
-
-
-DATABASES = {
-        'default': dj_database_url.parse('postgres://sscxvllufwjefu:8743a029bcf40fc3d6bb26840c4472ebd19de94e479331ca58146316f319ff28@ec2-54-220-14-54.eu-west-1.compute.amazonaws.com:5432/d9u6u4v6ienj5n', conn_max_age=600)
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 
