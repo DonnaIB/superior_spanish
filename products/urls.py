229d3113de0f6d861299e993aa075474635f8ca4
DonnaIB <donnaimrie@gmail.com>
2021-11-13 17:08:22 +0000

    Added conditioning so a user can't buy lifetime access product twice and updated styling on profile page

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_info, name='product_info')
+    path('<product_id>', views.product_info, name='product_info')    
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
-
+from profiles.models import UserProfile
+from checkout.models import Order
 # Create your views here.
 
 
@@ -46,9 +47,27 @@ def product_info(request, product_id):
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+    stories = False
+    lessons = False
+
+    for order in orders:
+        if order.product.id == 3:
+            print("3")
+            lessons = True
+
+        elif order.product.id == 4:
+            print("4")
+            stories = True
+
 
     context = {
         'product': product,
+        'orders': orders,
+        'lessons': lessons,
+        'stories': stories
+
     }
 
     return render(request, 'products/product_info.html', context)
