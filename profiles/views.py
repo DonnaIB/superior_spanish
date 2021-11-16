6f4547f78bf078959c9802227d3ce04778a611fc
DonnaIB <donnaimrie@gmail.com>
2021-11-16 13:37:01 +0000

    Added more security to profile pages & edited title of log out page

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import UserProfile
 from checkout.models import Order
 
@@ -26,10 +27,42 @@ def profile(request):
 @login_required
 def short_stories(request):
     """ A view to return the short stories page """
-    return render(request, 'profiles/recorded_lessons.html')    
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    orders = profile.orders.all()
+
+    for order in orders:
+        if order.product.id == 4:
+
+            template = 'profiles/short_stories.html'
+            context = {
+                'orders': orders,
+            }
+            return render(request, template, context)
+
+    messages.error(request, "Sorry! You don't have authorisation to view that page.")
+    return render(request, 'profiles/profile.html')
+
+
+
 
 @login_required
 def recorded_lessons(request):
     """ A view to return the recorded lessons page """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
-    return render(request, 'profiles/recorded_lessons.html')
+    orders = profile.orders.all()
+
+    for order in orders:
+        if order.product.id == 3:
+
+            template = 'profiles/recorded_lessons.html'
+            context = {
+                'orders': orders,
+            }
+            return render(request, template, context)
+
+    messages.error(request, "Sorry! You don't have authorisation to view that page.")
+    return render(request, 'profiles/profile.html')
+
+
